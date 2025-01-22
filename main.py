a812893f72367f4d9abe7c822dec024c6cc5cf1a
Kazuryy <luc.jacques72@gmail.com>
2025-01-22 16:55:19 +0100

    Add configure_router function (for packet tracer by Cisco)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ipaddress
 import math
 
+
 def calculate_network_address(ip, mask):
     network = ipaddress.IPv4Network(f"{ip}/{mask}", strict=False)
     return str(network.network_address)
@@ -19,6 +20,13 @@ def calculate_subnet_mask(ip, mask):
     network = ipaddress.IPv4Network(f"{ip}/{mask}", strict=False)
     return str(network.netmask)
 
+def is_valid_ip(ip):
+    try:
+        ipaddress.IPv4Address(ip)
+        return True
+    except ipaddress.AddressValueError:
+        return False
+
 def subnet_network(ip, mask, num_subnets):
     network = ipaddress.IPv4Network(f"{ip}/{mask}", strict=False)
     subnets = list(network.subnets(new_prefix=network.prefixlen + (num_subnets - 1).bit_length()))
@@ -31,10 +39,33 @@ def subnet_network(ip, mask, num_subnets):
     return subnet_info
 
 def main():
-    ip = input("Entrer une adresse IP (e.g., 192.168.1.0): ")
-    mask = int(input("Entrer le masque de sous-réseau (e.g., 24): "))
-    num_subnets = int(input("Combien de sous-réseaux voulez-vous: "))
-    
+    while True:
+        ip = input("Entrer une adresse IP (e.g., 192.168.1.0): ")
+        if is_valid_ip(ip):
+            break
+        else:
+            print("Adresse IP invalide. Veuillez réessayer.")
+
+    while True:
+        try:
+            mask = int(input("Entrer le masque de sous-réseau (e.g., 24): "))
+            if 0 <= mask <= 32:
+                break
+            else:
+                print("Masque de sous-réseau invalide. Veuillez entrer un nombre entre 0 et 32.")
+        except ValueError:
+            print("Masque de sous-réseau invalide. Veuillez entrer un nombre entre 0 et 32.")
+    # Ptn c'est long tout ça
+    while True:
+        try:
+            num_subnets = int(input("Combien de sous-réseaux voulez-vous: "))
+            if num_subnets > 0:
+                break
+            else:
+                print("Le nombre de sous-réseaux doit être supérieur à 0.")
+        except ValueError:
+            print("Nombre de sous-réseaux invalide. Veuillez entrer un nombre entier.")
+    # 
     vlan_tab =[]
     for i in range(num_subnets):
         namevlan = str(input("Entrer le nom du vlan: "))
@@ -43,29 +74,29 @@ def main():
 
     network_address = calculate_network_address(ip, mask)
     broadcast_address = calculate_broadcast_address(ip, mask)
+    ip_mask = calculate_subnet_mask(ip, mask)
     subnets = subnet_network(ip, mask, num_subnets)
-
+    print(subnets)
     print(f"\nAdresse Réseau: {network_address}")
     print(f"Adresse Broadcast: {broadcast_address}")
     print("Sous-réseaux:\n")
-    # for i, (net, broad, router, switch) in enumerate(subnets):
-    #     print(i)
-    #     vlan = vlan_tab[i-1][1]
-    #     print(f"  Sous Réseau {i+1}:")
-    #     print(f"    Numéro du lan: vlan {i+1}0")
-    #     print(f"    Adresse Réseau: {net}")
-    #     print(f"    Adresse Broadcast: {broad}")
-    #     print(f"    IP du Routeur: {router}")
-    #     print(f"    IP du Switch: {switch}\n")
-    #     print("Configuration du Routeur\n")
-    #     print(configure_switch(router, vlan, num_subnets))
-    #     print("Configuration du Switch:\n")
-    #     print(configure_router(switch, vlan))
-    print(ip, mask)
-    configure_switch(subnets, vlan_tab, ip, mask)
+    for i, (net, broad, router, switch) in enumerate(subnets):
+        print(f"  Sous Réseau {i+1}:")
+        print(f"    Numéro du lan: vlan {i+1}0")
+        print(f"    Adresse Réseau: {net}")
+        print(f"    Adresse Broadcast: {broad}")
+        print(f"    IP du Routeur: {router}")
+        print(f"    IP du Switch: {switch}\n")
 
-def configure_switch(subnets, vlan_tab, ip, mask):
-    print(ip, mask)
+    # Config ou pas du switch et routeur
+    choix_config_switch = input("Voulez-vous configurer les switchs et routeurs? (O/N): ")
+    if choix_config_switch.lower() == "o":
+        configure_switch(subnets, vlan_tab, ip_mask)
+    choix_config_router = input("Voulez-vous configurer les switchs et routeurs? (O/N): ")
+    if choix_config_router.lower() == "o":
+        configure_router(subnets, vlan_tab, ip_mask)
+
+def configure_switch(subnets, vlan_tab, ip_mask):
     with open("switch_config.txt", "w") as file:
         file.write("enable\nconf t\n")
         # Création des vlan
@@ -74,7 +105,6 @@ def configure_switch(subnets, vlan_tab, ip, mask):
             file.write(f"name {vlan_tab[i][0]}\n")
         file.write("exit\n")
         # Configuration des vlan
-        ip_mask = calculate_subnet_mask(ip, mask)
         for i in range(len(vlan_tab)):
             file.write(f"interface vlan {vlan_tab[i][1]}\n")
             switch_ip = subnets[i][3]
@@ -90,7 +120,18 @@ def configure_switch(subnets, vlan_tab, ip, mask):
         file.write(f"interface {port_routeur}\n")
         file.write("switchport mode trunk\n")
         file.write("end\n")
- 
+
+def configure_router(subnets, vlan_tab, ip_mask):
+    with open("router_config.txt", "w") as file:
+        file.write("enable\nconf t\n")
+        # Création des vlan
+        for i in range(len(vlan_tab)):
+            file.write(f"interface gi0/0.{vlan_tab[i][1]}\n")
+            file.write(f"encapsulation dot1Q {vlan_tab[i][1]}\n")
+            file.write(f"ip address {subnets[i][2]} {ip_mask}\n")
+        file.write("interface gi0/0\n")
+        file.write("no shutdown\n")
+
 
 if __name__ == "__main__":
     main()
